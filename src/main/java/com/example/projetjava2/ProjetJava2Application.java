f5f713085c6ac1d22ebbca802414bf19d7288c66
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-11-23 16:25:19 +0100

    first commit

---
+++ src/main/java/com/example/projetjava2/ProjetJava2Application.java
@@ -0,0 +1,13 @@
+package com.example.projetjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJava2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetjava2/ProjetJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetjava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

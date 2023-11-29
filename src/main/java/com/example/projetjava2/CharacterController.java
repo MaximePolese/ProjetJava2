1afe623a3fb94b6e5381c4937003e2062b5cc40c
Maxime Polese <maxime.polese@le-campus-numerique.fr>
2023-11-29 14:45:04 +0100

    Mise en place des validations

--- src/main/java/com/example/projetjava2/CharacterController.java
+++ src/main/java/com/example/projetjava2/CharacterController.java
@@ -11,9 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

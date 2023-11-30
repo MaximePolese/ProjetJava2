0d23c8c43b876a7298505e42145bf81ba94f30f1
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-11-30 16:09:01 +0100

    Mise en place de Eureka avec toutes les connections

--- src/main/java/com/example/projetjava2/ProjetJava2Application.java
+++ src/main/java/com/example/projetjava2/ProjetJava2Application.java
@@ -2,8 +2,10 @@ package com.example.projetjava2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProjetJava2Application {
 
 	public static void main(String[] args) {
